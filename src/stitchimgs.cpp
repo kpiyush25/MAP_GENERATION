d9c2ed79704006e7ef6aef2630e5608c1a78ae69
kpiyush25 <piyushk@iitk.ac.in>
2021-07-19 01:46:53 +0530

    Add comments and clean the code

--- src/stitchimgs.cpp
+++ src/stitchimgs.cpp
@@ -1,65 +1,52 @@
-#include <fstream>
+#include "opencv2/highgui.hpp"    // for OpenCV's GUI modules
+#include "opencv2/imgcodecs.hpp"  // provides methods to read and write images
+#include "opencv2/stitching.hpp"  // header that provides the feature of image stitching
+#include <fstream>                // including this allows us to create files, write information to existing files
+                                  // and read information from the files
 #include <iostream>
-// Include header files from OpenCV directory
-// required to stitch images.
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/stitching.hpp"
 
 using namespace std;
 using namespace cv;
 
-// Define mode for stitching as panoroma
-// (One out of many functions of Stitcher)
 Stitcher::Mode mode = Stitcher::SCANS;
 
-// Array for pictures
-vector<Mat> imgs;
-
+vector<Mat> imgs;  // Vector for storing the pictures
 int main(int argc, char* argv[]) {
     int i;
+    // Now storing all the images taken by the drone into a vector in order to use the required OpenCV functions
     for (i = 0; i < 1000; ++i) {
-        // Read the ith argument or image
-        // and push into the image array
         std::string gray_image = "/home/piyush/images/Gray_Image";
-        std::string num_images = std::to_string(i);
-        gray_image.append(num_images);
+        std::string count = std::to_string(i);
+        gray_image.append(count);
         gray_image.append(".jpg");
         Mat img = imread(gray_image);
         if (img.empty()) {
-            // Exit if image is not present
             cout << "Can't read image "
                  << "\n";
-            // return -1;
             break;
         }
         imgs.push_back(img);
         cout << imgs.size() << "\n";
     }
     cout << i << endl;
-    // Define object to store the stitched image
-    Mat stitched_img;
+    Mat stitched_img;  // this object will store the stitched image
 
-    // Create a Stitcher class object with mode scans.
-    Ptr<Stitcher> stitcher = Stitcher::create(mode, false);
+    Ptr<Stitcher> stitcher = Stitcher::create(mode, false);  // Creating a Stitcher class object with mode scans.
 
-    // Command to stitch all the images present in the image array
+    // this will stitch all the images present in the image array
     Stitcher::Status status = stitcher->stitch(imgs, stitched_img);
 
     if (status != Stitcher::OK) {
-        // Check if images could not be stiched
-        // status is OK if images are stiched successfully
+        // if the images are not stitched successfully then the status will not be OK
         cout << "Can't stitch images\n";
         return -1;
     }
 
-    // Store a new image stiched from the given
-    // set of images as "result.jpg"
+    // Now storing the stitched image as result.jpg
     imwrite("result.jpg", stitched_img);
 
-    // Show the result
-    imshow("Result", stitched_img);
+    imshow("Result", stitched_img);  // displaying the stitched image
 
-    waitKey(0);
+    waitKey(0);  // when the parameter is 0 here then it shows the image window infinitely until we press any key
     return 0;
 }
